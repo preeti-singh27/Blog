4ea9e0eac24a8fc54e31a9df6764eb99c4cf4fe1
Preeti Singh <167978150+preeti-singh27@users.noreply.github.com>
2024-10-28 02:09:38 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
   host:"localhost",
   user:"root",
-  password:"d8b10770",
+  password:"your password",
 //  password: process.env.DB_KEY,
   database:"blog"
 })
